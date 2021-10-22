c7fcb1b130ea6301ae055e9a60c3808d5647611c
Richard Albornoz <richard_albor4@hotmail.com>
2021-10-21 22:53:08 -0300

    Item.js and ItemList.js

--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -3,8 +3,7 @@ import './ItemListContainer.css';
 const ListContainer =(props)=> {
     return(
         <div className="listaProductos">
-            <div>{props.text}</div>
-            {props.children}
+            <div>{props.greeting}</div>
         </div>
     )
 }
