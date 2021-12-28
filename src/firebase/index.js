e07e22b7a3c5d10aac8d55f6304806f8b710f0c3
Richard Albornoz <richard_albor4@hotmail.com>
2021-12-28 10:37:23 -0300

    firebase

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -2,18 +2,15 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBtvmImIR-mWMwxRdUg8hvw7iwDVMM74zA",
-    authDomain: "absolut-ecommerce.firebaseapp.com",
-    projectId: "absolut-ecommerce",
-    storageBucket: "absolut-ecommerce.appspot.com",
-    messagingSenderId: "139924492295",
-    appId: "1:139924492295:web:e821fe7222f44fb9665aa2",
-    // eslint-disable-next-line no-template-curly-in-string
-    measurementId: "${config.measurementId}"
-  };
-  
-  const app = initializeApp(firebaseConfig);
+  apiKey: "AIzaSyChVCz3oqRGiF6hPBNBf8nn0LN1sRr10r8",
+  authDomain: "absolut-ecommerce2.firebaseapp.com",
+  projectId: "absolut-ecommerce2",
+  storageBucket: "absolut-ecommerce2.appspot.com",
+  messagingSenderId: "555345304015",
+  appId: "1:555345304015:web:5f65a7d6683111f56c5aba"
+};
 
-  export const getFirebase = () => app;
+const app = initializeApp(firebaseConfig);
+export const getFirebase = () => app;
 
   export { getFirestore };
